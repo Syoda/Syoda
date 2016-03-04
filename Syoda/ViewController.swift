4e39195510b8d73f944d24befea0f71d97802144
Aditya Balwani <adibalwani@gmail.com>
2016-03-03 22:45:30 -0500

    Initial Commit

---
+++ Syoda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Syoda
+//
+//  Created by Aditya Balwani on 3/3/16.
+//  Copyright © 2016 Syoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
